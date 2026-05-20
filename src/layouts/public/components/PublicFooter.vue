35398eddd24662a057583ec7970ebef61353f023
axeljavier-alvarez <axjavier4alvarez44@gmail.com>
2026-05-20 11:50:32 -0600

    componentes reutilizables

---
+++ src/layouts/public/components/PublicFooter.vue
@@ -0,0 +1,103 @@
+<template>
+    <!-- FOOTER -->
+    <footer class="bg-neutral-primary-soft mt-5">
+        <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+
+            <div class="md:flex md:justify-between">
+
+                <div class="mb-6 md:mb-0">
+                    <a href="https://flowbite.com/" class="flex items-center">
+                        <img
+                            src="https://flowbite.com/docs/images/logo.svg"
+                            class="h-7 me-3"
+                            alt="FlowBite Logo"
+                        />
+
+                        <span class="text-heading self-center text-2xl font-semibold whitespace-nowrap">
+                            Flowbite
+                        </span>
+                    </a>
+                </div>
+
+                <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+
+                    <div>
+                        <h2 class="mb-6 text-sm font-semibold text-heading uppercase">
+                            Resources
+                        </h2>
+
+                        <ul class="text-body font-medium">
+                            <li class="mb-4">
+                                <a href="https://flowbite.com/" class="hover:underline">
+                                    Flowbite
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="https://tailwindcss.com/" class="hover:underline">
+                                    Tailwind CSS
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h2 class="mb-6 text-sm font-semibold text-heading uppercase">
+                            Follow us
+                        </h2>
+
+                        <ul class="text-body font-medium">
+                            <li class="mb-4">
+                                <a href="#" class="hover:underline">
+                                    Github
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#" class="hover:underline">
+                                    Discord
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h2 class="mb-6 text-sm font-semibold text-heading uppercase">
+                            Legal
+                        </h2>
+
+                        <ul class="text-body font-medium">
+                            <li class="mb-4">
+                                <a href="#" class="hover:underline">
+                                    Privacy Policy
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#" class="hover:underline">
+                                    Terms & Conditions
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+
+            <hr class="my-6 border-default sm:mx-auto lg:my-8" />
+
+            <div class="sm:flex sm:items-center sm:justify-between">
+
+                <span class="text-sm text-body sm:text-center">
+                    © 2023 Flowbite™. All Rights Reserved.
+                </span>
+
+                <div class="flex mt-4 sm:justify-center sm:mt-0">
+                    <!-- ICONOS -->
+                </div>
+
+            </div>
+
+        </div>
+    </footer>
+</template>
---
+++ src/layouts/public/components/PublicNav.vue
@@ -0,0 +1,51 @@
+<template>
+    
+<nav class="bg-neutral-primary fixed w-full z-20 top-0 start-0 border-b border-default">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+
+            <a href="#" class="flex items-center space-x-3">
+                <img
+                    src="https://flowbite.com/docs/images/logo.svg"
+                    class="h-7"
+                    alt="Logo"
+                />
+
+                <span class="self-center text-xl text-heading font-semibold whitespace-nowrap">
+                    Flowbite
+                </span>
+            </a>
+
+            <div class="flex md:order-2">
+                <button
+                    type="button"
+                    class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-4 py-2"
+                >
+                    Login
+                </button>
+            </div>
+
+            <div class="hidden md:flex md:w-auto">
+                <ul class="flex flex-row gap-8 font-medium">
+                    <li>
+                        <a href="#" class="text-heading hover:text-blue-500">
+                            Home
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="text-heading hover:text-blue-500">
+                            About
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="text-heading hover:text-blue-500">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+</template>
