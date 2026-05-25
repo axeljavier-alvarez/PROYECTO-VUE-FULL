f9efb12a12ecb0e378bf9ca845497e503e7db5c2
axeljavier-alvarez <axjavier4alvarez44@gmail.com>
2026-05-25 07:53:18 -0600

    cambios

--- src/layouts/admin/components/AdminAside.vue
+++ src/layouts/admin/components/AdminAside.vue
@@ -1,4 +1,5 @@
 <script setup>
+import AdminNavLink from './AdminNavLink.vue';
 const openSidebar = defineModel();
 </script>
 
@@ -22,28 +23,15 @@ const openSidebar = defineModel();
 
             <ul class="space-y-2 font-medium">
                 <li>
-                    <RouterLink
-                        :to="{ name: 'admin.dashboard' }"
-                        active-class="bg-gray-100 text-gray-900"
-                        class="flex items-center px-2 py-1.5 rounded-base hover:bg-gray-100 transition-colors"
-                    >
-                        <svg
-                            class="w-5 h-5"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                        >
-                            <path
-                                stroke="currentColor"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M10 6.025A7.5 7.5 0 1 0 17.975 14H10V6.025Z"
-                            />
-                        </svg>
-
+                    <AdminNavLink :to="{
+                        name: 'admin.dashboard'
+                    }">
+                        <span class="inline-flex w-6 h-6 justify-center items-center">
+                            <i class="fa-solid fa-gauge-high"></i>
+                        </span>
                         <span class="ms-3">Dashboard</span>
-                    </RouterLink>
+                    </AdminNavLink>
+                    
                 </li>
             </ul>
         </div>
---
+++ src/layouts/admin/components/AdminNavLink.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+<template>
+    <RouterLink 
+        active-class="bg-gray-100 text-gray-900"
+        class="flex items-center px-2 py-1.5 rounded-base hover:bg-gray-100 transition-colors">
+        <slot>
+
+        </slot>
+    </RouterLink>
+</template>
