90d50b9d846ab3a69fb052d7643ed824efbaffa0
axeljavier-alvarez <axjavier4alvarez44@gmail.com>
2026-05-27 13:34:10 -0600

    informacion auth

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <script setup lang="ts">
-import {  RouterView } from 'vue-router'
+import { onMounted } from 'vue';
+import {  RouterView } from 'vue-router';
+
+import { useAuthStore } from './modules/auth/stores/authStore';
+
+const authStore = useAuthStore();
+
+onMounted(async() => {
+  try {
+    await authStore.fetchUser();
+
+    console.log(authStore.user);
+  } catch(error){
+    console.log(error);
+  }
+})
 // console.log(localStorage.getItem('access_token'))
 </script>
 
--- src/modules/auth/stores/authStore.js
+++ src/modules/auth/stores/authStore.js
@@ -3,11 +3,10 @@ import authService from "../services/authServices";
 import { ref, computed } from "vue";
 
 export const useAuthStore = defineStore('auth', () => {
-
-
+    const _user = ref(null);
     const token = ref(localStorage.getItem('access_token') || null);
     const isAuthenticated = computed(() => !!token.value);
-
+    const user = computed(() => _user.value);
 
     async function login(credentials){
         
@@ -15,6 +14,9 @@ export const useAuthStore = defineStore('auth', () => {
             const response = await authService.login(credentials);
             localStorage.setItem('access_token', response.access_token);
             token.value = response.access_token;
+            await fetchUser();
+
+            return response;
         } catch (error){
             throw error; 
         }
@@ -23,16 +25,32 @@ export const useAuthStore = defineStore('auth', () => {
     async function logout(){
         try {
             await authService.logout();
-            localStorage.removeItem('access_token');
-            token.value = null;
+            
         } catch(error){
             throw error;
+        } finally {
+            localStorage.removeItem('access_token');
+            token.value = null;
+        }
+    }
+
+    async function fetchUser(){
+        if (isAuthenticated.value){
+            try {
+                _user.value = await authService.me();
+            } catch (error){
+                logout();
+                throw error;
+            }
         }
     }
 
     return {
         isAuthenticated, 
         login,
-        logout
+        logout,
+        user,
+        token,
+        fetchUser
     };
 });
